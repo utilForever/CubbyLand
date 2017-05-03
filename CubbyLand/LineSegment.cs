955bbd8fee5bafa8e852620f64d0c79973da4613
Chris Ohk <utilforever@gmail.com>
2017-05-04 00:18:21 +0900

    Implement LineSegment class

--- CubbyLand/LineSegment.cs
+++ CubbyLand/LineSegment.cs
@@ -15,5 +15,27 @@ namespace CubbyLand
             this.p0 = p0;
             this.p1 = p1;
         }
+
+        public static double CompareLengths_MAX(LineSegment s0, LineSegment s1)
+        {
+            double l0 = Point.Distance(s0.p0, s0.p1);
+            double l1 = Point.Distance(s1.p0, s1.p1);
+
+            if (l0 < l1)
+            {
+                return 1;
+            }
+            if (l0 > l1)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        public static double CompareLength(LineSegment e0, LineSegment e1)
+        {
+            return -CompareLengths_MAX(e0, e1);
+        }
     }
 }
