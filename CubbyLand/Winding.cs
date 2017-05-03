5c05f0b804443af75aa8e0178705aaddc546fba2
Chris Ohk <utilforever@gmail.com>
2017-05-04 01:26:31 +0900

    Implement Winding class

---
+++ CubbyLand/Winding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubbyLand
+{
+    public class Winding
+    {
+        public static Winding CW = new Winding("Clockwise");
+        public static Winding CCW = new Winding("Counterclockwise");
+        public static Winding NONE = new Winding("None");
+        private string name;
+
+        private Winding(string name)
+        {
+            this.name = name;
+        }
+    }
+}
