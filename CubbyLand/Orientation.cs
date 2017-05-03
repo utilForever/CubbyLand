618ceed000bf6a5434c765226a99132405aeb720
Chris Ohk <utilforever@gmail.com>
2017-05-03 19:47:09 +0900

    Add Orientation class

---
+++ CubbyLand/Orientation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubbyLand
+{
+    class Orientation
+    {
+        public static Orientation CW = new Orientation("CW");
+        public static Orientation CCW = new Orientation("CCW");
+        public string name;
+
+        public Orientation(string name)
+        {
+            this.name = name;
+        }
+
+        public static Orientation Other(Orientation orientation)
+        {
+            return (orientation == CW ? CCW : CW);
+        }
+    }
+}
