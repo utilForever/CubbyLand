120787891182330b2401a40052e273cf521822ca
Chris Ohk <utilforever@gmail.com>
2017-05-03 19:34:53 +0900

    Add Point class

---
+++ CubbyLand/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubbyLand
+{
+    class Point
+    {
+        public double x;
+        public double y;
+
+        public static double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+        }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double L2()
+        {
+            return (x * x) + (y * y);
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt((x * x) + (y * y));
+        }
+    }
+}
