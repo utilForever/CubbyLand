464aba0826644400364905242ed85cc0f71ee78e
Chris Ohk <utilforever@gmail.com>
2017-05-04 00:40:50 +0900

    Implement Rectangle class

--- CubbyLand/Rectangle.cs
+++ CubbyLand/Rectangle.cs
@@ -16,5 +16,32 @@ namespace CubbyLand
         public double bottom;
         public double left;
         public double top;
+
+        public Rectangle(double x, double y, double width, double height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.left = x;
+            this.top = y;
+            this.right = x + width;
+            this.bottom = y + height;
+        }
+
+        public bool LiesOnAxes(Point p)
+        {
+            return ((Math.Abs(p.x - x) <= 1) || (Math.Abs(p.y - y) <= 1));
+        }
+
+        public bool InBounds(Point p)
+        {
+            return InBounds(p.x, p.y);
+        }
+
+        public bool InBounds(double x, double y)
+        {
+            return !(x < this.x || x > right || y < this.y || y > bottom);
+        }
     }
 }
