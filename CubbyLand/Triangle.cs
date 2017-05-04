70f44fc5d4795e52f40aa1f59feb5d1f857017bb
Chris Ohk <utilforever@gmail.com>
2017-05-04 22:44:23 +0900

    Upload Triangle.cs - Implement Triangle class

---
+++ CubbyLand/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubbyLand
+{
+    public class Triangle
+    {
+        private List<Site> sites;
+
+        public Triangle(Site a, Site b, Site c)
+        {
+            sites = new List<Site>();
+            sites.Add(a);
+            sites.Add(b);
+            sites.Add(c);
+        }
+
+        public List<Site> GetSites()
+        {
+            return sites;
+        }
+
+        public void Dispose()
+        {
+            sites.Clear();
+            sites = null;
+        }
+    }
+}
