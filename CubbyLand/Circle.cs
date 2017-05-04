74552ec43a1b160b73ed0faa7a6253d0413fc5ab
Chris Ohk <utilforever@gmail.com>
2017-05-04 22:50:07 +0900

    Upload Circle.cs - Implement Circle class

---
+++ CubbyLand/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubbyLand
+{
+    public class Circle
+    {
+        public Point center;
+        public double radius;
+
+        public Circle(double centerX, double centerY, double radius)
+        {
+            this.center = new Point(centerX, centerY);
+            this.radius = radius;
+        }
+    }
+}
