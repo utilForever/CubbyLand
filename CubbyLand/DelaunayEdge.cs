c157b6bb0debe6c2e710061ff3fc81001414eac6
Chris Ohk <utilforever@gmail.com>
2017-05-05 13:37:19 +0900

    Update DelaunayEdge.cs - Implement DelaunayEdge class

--- CubbyLand/DelaunayEdge.cs
+++ CubbyLand/DelaunayEdge.cs
@@ -8,6 +8,18 @@ namespace CubbyLand
 {
     public class DelaunayEdge
     {
+        public int index;
+        public Center d0, d1;
+        public Corner v0, v1;
+        public Point midPoint;
+        public int river;
 
+        public void SetVoronoi(Corner v0, Corner v1)
+        {
+            this.v0 = v0;
+            this.v1 = v1;
+
+            midPoint = new Point((v0.loc.x + v1.loc.x) / 2, (v0.loc.y + v1.loc.y) / 2);
+        }
     }
 }
