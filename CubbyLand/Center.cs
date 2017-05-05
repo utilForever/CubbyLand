5f941d78242a3da6289f1d3a3cba9f1c6bbe981d
Chris Ohk <utilforever@gmail.com>
2017-05-05 13:32:10 +0900

    Update Center.cs - Implement Center class

--- CubbyLand/Center.cs
+++ CubbyLand/Center.cs
@@ -8,5 +8,28 @@ namespace CubbyLand
 {
     public class Center
     {
+        public int index;
+        public Point loc;
+        public List<Corner> corners = new List<Corner>();
+        public List<Center> neighbors = new List<Center>();
+        public List<DelaunayEdge> borders = new List<DelaunayEdge>();
+        public bool border;
+        public bool ocean;
+        public bool water;
+        public bool coast;
+        public double elevation;
+        public double moisture;
+        public Enum biome;
+        public double area;
+
+        public Center()
+        {
+
+        }
+
+        public Center(Point loc)
+        {
+            this.loc = loc;
+        }
     }
 }
