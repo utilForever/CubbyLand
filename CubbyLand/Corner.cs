db6caa79c6041f04a5f67dfbaa3f036cfd025b77
Chris Ohk <utilforever@gmail.com>
2017-05-05 13:26:21 +0900

    Upload Corner.cs - Implement Corner class

---
+++ CubbyLand/Corner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubbyLand
+{
+    public class Corner
+    {
+        public List<Center> touches = new List<Center>();
+        public List<Corner> adjacent = new List<Corner>();
+        public List<DelaunayEdge> protrudes = new List<DelaunayEdge>();
+        public Point loc;
+        public int index;
+        public bool border;
+        public double elevation;
+        public bool water;
+        public bool ocean;
+        public bool coast;
+        public Corner downslope;
+        public int river;
+        public double moisture;
+    }
+}
